0047e421ec8699987f8ec11e0a403faf01c7f0b9
JustinGaj <jtg0825@yahoo.com>
2024-09-23 21:18:34 -0400

    changes

--- main.py
+++ main.py
@@ -30,8 +30,7 @@ class Block():
         )
 
     def __str__(self):
-        return str("Block#: %s\nHash: %s\nPrevious:" + 
-                "%s\nData: %s\nNonce: %s\n" %(
+        return str("Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n" %(
                 self.number, 
                 self.hash(), 
                 self.previous_hash, 
@@ -48,31 +47,43 @@ class Blockchain():
         self.chain = chain
     
     def add(self, block):
-        self.chain.append({
-            'hash': block.hash(), 
-            'previous': block.previous_hash, 
-            'number': block.number, 
-            'data': block.data, 
-            'nonce': block.nonce
-        })
+        self.chain.append(block)
+
+    def remove(self, block):
+        self.chain.remove(block)
     
     def mine(self, block):
         try:
-            block.previous_hash = self.chain[-1].get('hash')
+            block.previous_hash = self.chain[-1].hash()
         except IndexError:
             pass
 
         while True:
-            if block.hash()[:4] == "0" * difficulty:
+            if block.hash()[:self.difficulty] == "0" * self.difficulty:
                 self.add(block); break
             else:
                 block.nonce += 1
 
+    def isValid(self):
+        for i in range(1, len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current = self.chain[i - 1].hash()
+            if _previous != _current or _current[:self.difficulty] != "0" * self.difficulty:
+                return False
+        return True
 
 def main():
     blockchain = Blockchain()
     database = ["hello world", "what's up", "hello", "bye"]
-    print(blockchain(database))
+    
+    num = 0
+    for data in database:
+        num += 1
+        blockchain.mine(Block(data, num))
+    for block in blockchain.chain:
+        print(block)
+
+    print(blockchain.isValid())
 
 
 if __name__ == '__main__':
